2e6964491cdf5c4be585213cef44c49ade06a67f
Jaydippatel <macbook@MacBooks-MacBook-Pro.local>
2022-09-25 10:43:48 +0530

    add testnet

--- src/LensProtocol/services/Apollo_Client.js
+++ src/LensProtocol/services/Apollo_Client.js
@@ -1,27 +1,17 @@
-// this is showing you how you use it with react for example
-// if your using node or something else you can import using
-// @apollo/client/core!
+
 import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client'
 
 const APIURL = 'https://api-mumbai.lens.dev/'
 // const APIURL = 'https://api.lens.dev';
 
-const httpLink = new HttpLink({ uri: APIURL });
-
-// example how you can pass in the x-access-token into requests using `ApolloLink`
-const authLink = new ApolloLink((operation, forward) => {
-  // Retrieve the authorization token from local storage.
-  // if your using node etc you have to handle your auth different
-  const token = localStorage.getItem('accessToken');
-
-  // Use the setContext method to set the HTTP headers.
+const httpLink = new HttpLink({ uri: APIURL }); 
+const authLink = new ApolloLink((operation, forward) => { 
+  const token = localStorage.getItem('accessToken'); 
   operation.setContext({
     headers: {
       'x-access-token': token ? `Bearer ${token}` : ''
     }
-  });
-
-  // Call the next link in the middleware chain.
+  }); 
   return forward(operation);
 });
 
